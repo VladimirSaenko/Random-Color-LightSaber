e048066fb8c72a2fef0e0308fde19681613b1407
Vladimir Saenko <vova.saenko1@gmail.com>
2021-05-10 17:45:40 +0300

    add site

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+
+"use strict"
+
+getRandomColor();
+
+function getRandomColor() {
+    document.documentElement.style.setProperty('--blade-color', getRandomInt(10,1000));
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
